81b0bef78ceb706e4a3b7c4309f779e1b436956d
Alisha <alishasaxena@Alishas-MacBook-Air.local>
2024-03-21 13:57:36 +0530

    update ui

--- src/App.js
+++ src/App.js
@@ -46,6 +46,8 @@ const App = () => {
   };
 
   const handleResultClick = (title) => {
+    setSearchResults([]);
+
     if (!selectedBooks.includes(title)) {
       setSelectedBooks([...selectedBooks, title]);
     }
@@ -53,18 +55,42 @@ const App = () => {
 
   return (
     <div className="flex flex-col items-center h-screen gap-4 p-4 m-auto bg-pink-100 App">
-      <h1 className="text-sm font-bold md:text-xl">Book Search App</h1>
+      <h1 className="text-sm font-bold md:text-2xl head">Book Search App</h1>
       <div className="w-9/12 md:w-1/2">
         <input
           list="titles"
           placeholder="Search"
           value={searchText}
-          className="w-full px-6 py-2 text-sm border-2 border-black rounded-lg"
+          className="w-full px-6 py-2 text-sm border-2 border-black rounded-lg focus:outline-none"
           onChange={handleInputChange}
         />
       </div>
       <div className="flex flex-wrap items-center justify-center w-9/12 gap-4 py-4 md:w-1/2">
         {searchResults.map((title, index) => {
+          const color = getRandomColor();
+          return (
+            <div
+              key={index}
+              onClick={() => handleResultClick(title)}
+              className="w-full bg-white rounded-lg shadow-lg cursor-pointer hover:opacity-60"
+            >
+              <div className="flex flex-col items-center justify-center gap-4 p-4 ">
+                <div
+                  style={{ color: color }}
+                  className="flex items-center gap-2"
+                >
+                  <span className="material-symbols-outlined">
+                    auto_stories
+                  </span>
+                  {title}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="container grid gap-6 md:gap-12 md:grid-cols-4">
+        {selectedBooks.map((title, index) => {
           const bookIndex = booksData.titles.findIndex((t) => t === title);
           const summary = booksData.summaries[bookIndex];
           const author = booksData.authors[bookIndex];
@@ -72,10 +98,9 @@ const App = () => {
           return (
             <div
               key={index}
-              onClick={() => handleResultClick(title)}
-              className="bg-white cursor-pointer hover:opacity-60"
+              className="bg-white shadow-lg rounded-2xl hover:opacity-60"
             >
-              <div className="flex flex-col items-center justify-center gap-4 p-4 shadow-lg">
+              <div className="flex flex-col items-center justify-center gap-4 p-6 ">
                 <div
                   style={{ color: color }}
                   className="flex items-center gap-2"
@@ -85,34 +110,9 @@ const App = () => {
                   </span>
                   {title}
                 </div>
-                <p className="text-xs md:text-sm">{summary.summary}</p>
-                <p style={{ color: color }} className="text-xs md:text-sm">
-                  {author.author}
+                <p className="text-xs md:text-sm leading-1.5 ">
+                  {summary.summary}
                 </p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="grid gap-6 md:gap-12 md:grid-cols-4">
-        {selectedBooks.map((title, index) => {
-          const bookIndex = booksData.titles.findIndex((t) => t === title);
-          const summary = booksData.summaries[bookIndex];
-          const author = booksData.authors[bookIndex];
-          const color = getRandomColor();
-          return (
-            <div key={index} className="bg-white hover:opacity-60">
-              <div className="flex flex-col items-center justify-center gap-4 p-4 shadow-lg">
-                <div
-                  style={{ color: color }}
-                  className="flex items-center gap-2"
-                >
-                  <span className="material-symbols-outlined">
-                    auto_stories
-                  </span>
-                  {title}
-                </div>
-                <p className="text-xs md:text-sm">{summary.summary}</p>
                 <p style={{ color: color }} className="text-xs md:text-sm">
                   {author.author}
                 </p>
